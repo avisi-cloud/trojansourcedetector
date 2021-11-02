317b1e553ffb00da3acade9ccfbbd822624b405b
Janos <86970079+janosdebugs@users.noreply.github.com>
2021-11-02 21:27:31 +0100

    Better error message for test error

--- detector_test.go
+++ detector_test.go
@@ -1,13 +1,19 @@
 package trojansourcedetector_test
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/haveyoudebuggedit/trojansourcedetector"
 )
 
+//go:generate go run cmd/testdata/main.go
+
 func TestE2E(t *testing.T) {
+	assertFileExists(t, "testdata/bidi.txt")
+	assertFileExists(t, "testdata/unicode.txt")
+
 	detector := trojansourcedetector.New(&trojansourcedetector.Config{
 		Directory:     "testdata",
 		DetectUnicode: true,
@@ -20,6 +26,12 @@ func TestE2E(t *testing.T) {
 	assertHasError(t, errs, trojansourcedetector.ErrBIDI, "bidi.txt", 1, 44)
 	assertHasError(t, errs, trojansourcedetector.ErrUnicode, "unicode.txt", 1, 29)
 	assertHasError(t, errs, trojansourcedetector.ErrUnicode, "unicode.txt", 1, 30)
+}
+
+func assertFileExists(t *testing.T, file string) {
+	if _, err := os.Stat(file); err != nil {
+		t.Fatalf("file does not exist: %s did you run go generate? (%v)", file, err)
+	}
 }
 
 func assertHasError(
--- gen.go
+++
@@ -1,3 +0,0 @@
-package trojansourcedetector
-
-//go:generate go run cmd/testdata/main.go
